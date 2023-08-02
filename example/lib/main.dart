a442140a2f512e9e2b6b09eb30f10370026b50ae
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2023-08-02 11:39:13 +0700

    Init Platform & Adjusting Dependencies

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mercury_aichat_sdk_example/routest.dart';
+
+import 'chatbot/chatbot_binding.dart';
+import 'chatbot/chatbot_view.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: const Center(
-          child: Text('Running on:'),
-        ),
+    return GetMaterialApp(
+      title: 'Mercury AI-Chat SDK Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      getPages: [
+        GetPage(
+          name: Routes.chatbot,
+          page: () => const ChatbotView(),
+          binding: ChatbotBinding(),
+        ),
+      ],
     );
   }
 }
--- lib/mercury_aichat_sdk.dart
+++ lib/mercury_aichat_sdk.dart
@@ -1,1 +1,10 @@
 library mercury_aichat_sdk;
+
+export 'src/chatbot/controller/chatbot_controller.dart';
+export 'src/chatbot/data_source/chat_local_data_source.dart';
+export 'src/chatbot/models/chat_message_response.dart';
+export 'src/chatbot/models/chatbot_model.dart';
+export 'src/chatbot/repo/chatbot_repo.dart';
+export 'src/chatbot/usecases/create_conversation_usecase.dart';
+export 'src/chatbot/usecases/get_messages_usecase.dart';
+export 'src/chatbot/usecases/send_message_usecase.dart';
